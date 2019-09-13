8b42fc3c824154295706a61cfd98bfeddc25ab67
Bernardo Senna <bermullet@hotmail.com>
2019-09-13 12:55:05 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+# Urna-Eletronica
+
+Sistema de Urna eletrônica desenvolvido para a matéria de Engenharia de Software 1 (UFMG)
+
+## Instalação
+
+- Instale o banco de dados mariadb:
+```shell
+$ sudo apt-get install mariadb
+```
+
+- Clone o repositório utilizando o comando:
+```shell
+$ git clone git@github.com:bernardo-senna/Urna-Eletronica.git
+```
+
+- Entre na pasta do repositório e execute o script para criar o banco:
+```shell
+$ mysql -u root < create_db.sql
+```
+*Obs 1: Esse script remove o banco e o cria novamente, inserindo  alguns dados iniciais.*
+
+*Obs 2: Você pode executar esse script várias vezes, porém perderá os seus dados do banco.*
+
+- Adicione o projeto no NetBeans. Não testei em outra IDE, mas acredito que você não vai ter problemas se tentar.
---
+++ create_db.sql
@@ -0,0 +1,37 @@
+DROP DATABASE urna_eletronica;
+CREATE DATABASE urna_eletronica;
+USE urna_eletronica;
+
+DROP TABLE IF EXISTS pessoas;
+CREATE TABLE pessoas (
+  id INT NOT NULL AUTO_INCREMENT,
+  tipo INT NOT NULL,
+  nome VARCHAR(255) NOT NULL,
+  cpf VARCHAR(255) NOT NULL,
+  titulo_eleitor INT NOT NULL,
+  telefone VARCHAR(255) NOT NULL,
+  data_nascimento DATE NOT NULL,
+  endereco VARCHAR(255) NOT NULL,
+  especialidade_id INT DEFAULT NULL,
+  FOREIGN KEY (especialidade_id) REFERENCES especialidades(id),
+  PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS profissao;
+CREATE TABLE profissao (
+  id INT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(255) NOT NULL,
+  senha_fiscal VARCHAR(255) NULL,
+  PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS urnas;
+CREATE TABLE urnas (
+  id INT NOT NULL AUTO_INCREMENT,
+  local_voto VARCHAR(255) NOT NULL,
+  id_pessoa INT DEFAULT NULL,
+  id_profissao INT DEFAULT NULL,
+  FOREIGN KEY (id_pessoa) REFERENCES pessoas(id),
+  FOREIGN KEY (id_profissao) REFERENCES profissao(id),
+  PRIMARY KEY (id)
+);
